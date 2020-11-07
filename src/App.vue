7c47c33f8940342cc6be884ad702cae97fe58e28
kawinpop <kawinpop123@gmail.com>
2020-11-07 23:08:01 +0700

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<div>
+    <h1>v-model</h1>
+    <input v-model="massage" type="text" placeholder="Enter your massage">
+    <div>{{massage}}</div>
+    <button @click="massage = ''">Reset</button>
+
+    <router-view></router-view>
+</div>
 </template>
 
+<script>
+export default {
+    name: "app",
+    data() {
+        return {
+            massage: ""
+        }
+    },
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
